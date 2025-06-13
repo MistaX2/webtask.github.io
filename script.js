fb2a549620ccd0dc3969a18a1005e35514d1ddbe
Mr.-x- <mistax2.0z@gmail.com>
2025-06-13 19:00:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["YouTuber", "Developer", "", "Designer", "Phone repairs and other"],
+        strings: ["YouTuber", "Developer", "", "Designer", "Phone repairs and other","computer hardware technician"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["YouTuber", "Developer", "", "Designer", "Phone repairs and other"],
+        strings: ["YouTuber", "Developer", "", "Designer", "Phone repairs and other","computer hardware technician"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
